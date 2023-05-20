ba3db38bdf7e4f20d9986f391ee67e63710dce4a
elitonluiz1989 <elitonluiz1989@gmail.com>
2023-05-19 22:07:03 -0400

    Add project files.

---
+++ FuelConsumption.Api/Controllers/FuelConsumptionController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FuelConsumption.Api.Controllers
+{
+    [Route("api/fuel-consumption")]
+    [ApiController]
+    public class FuelConsumptionController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("It's working!");
+        }
+    }
+}
